d9d03cb179077448c6f2c6fe1c8820ad795b2d21
Technoboy10 <technoboy10@fake.email>
2013-07-19 21:49:34 -0600

    yay finished

--- snap2speech.py
+++ snap2speech.py
@@ -6,8 +6,8 @@ class CORSHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	print path
 	ospath = os.path.abspath('')
 	if 'say' in path:
-		regex = re.compile("\/say([A-Za-z0-9]+)")
-		m = regex.match(path)
+		regex = re.compile("\/say(.*)")
+		m = regex.match(urllib2.unquote(path))
 		engine.say(m.group(1))
 	engine.runAndWait()
 if __name__ == "__main__":
@@ -15,7 +15,8 @@ if __name__ == "__main__":
     import re
     import SocketServer
     import pyttsx
-    PORT = 5959 #T+T+S twice
+    import urllib2
+    PORT = 59591 #T+T+S twice
     engine = pyttsx.init()
     Handler = CORSHTTPRequestHandler
     #Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
